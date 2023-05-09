b0d255eaed341640c0d001adf35e5e24e50879d3
Tolgahan Ayaz <tolgahanayaz0@gmail.com>
2023-05-09 16:47:24 +0300

    whisper end

--- pages/api/whisper.ts
+++ pages/api/whisper.ts
@@ -7,3 +7,37 @@ bodyParser:false,
 },
 };
 
+export default withFileUpload(async(req,res)=>{
+    const file = req.file;
+    if(!file) {
+        res.status(400).send('No file Uploaded')
+        return;
+    }
+
+    //Create from data
+
+    const formData = new FormData();
+    formData.append('file',createReadStream(file.filepath),'audio.wav');
+    formData.append('model','whisper-1');
+    const response = await fetch(
+        'https://api.openai.com/v1/audio/transcriptions',
+        {
+            method: 'POST',
+            headers: {
+                ...formData.getHeaders(),
+                Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+            },
+            body:formData,
+        }
+    ) ;
+
+    const {text,error} = await response.json();
+    if(response.ok) {
+        res.status(200).json({text:text});
+
+    } else {
+        console.log('OPEN AI ERROR');
+        console.log(error.message);
+        res.status(400).send(new Error());
+    }
+});
